8ef51101734e9b01e32f1a48b0c036919a7c82d3
mariannecowherd <mariannecowherd@gmail.com>
2023-11-13 14:10:22 -0800

    add figure notebooks

---
+++ code/dirs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import os
+
+## define directories
+basedir = '/global/cfs/cdirs/m4099/fate-of-snotel/'
+projectdir = basedir + 'wrfdata/'
+snoteldir = basedir + 'snoteldata/'
+wrfdir = projectdir
+coorddir = projectdir + 'meta/meta_new/'
